f1f42a8261d017b6468821b584bcdbe405bce3f0
AdamMCG97 <adammorley8@aol.com>
2020-06-14 16:25:26 +0100

    Update neo4j bolt driver

--- src/main/java/tech/amcg/LLF/Application.java
+++ src/main/java/tech/amcg/LLF/Application.java
@@ -2,8 +2,12 @@ package tech.amcg.llf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableNeo4jRepositories(basePackages = "tech.amcg.llf")
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);

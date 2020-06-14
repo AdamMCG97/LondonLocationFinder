6b92c4a419662a74176bb53354a7ea8fb4d4c75c
AdamMCG97 <adammorley8@aol.com>
2020-06-14 18:34:14 +0100

    Change import from fully qualified name

--- src/main/java/tech/amcg/LLF/configuration/Neo4JConfiguration.java
+++ src/main/java/tech/amcg/LLF/configuration/Neo4JConfiguration.java
@@ -3,19 +3,19 @@ package tech.amcg.llf.configuration;
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import tech.amcg.llf.repository.TubeRepository;
+import org.neo4j.ogm.config.Configuration;
 
-@Configuration
+@org.springframework.context.annotation.Configuration
 @ComponentScan("tech.amcg.llf")
 @EnableNeo4jRepositories(basePackages="tech.amcg.llf.repository")
 public class Neo4JConfiguration {
 
     @Bean
-    public org.neo4j.ogm.config.Configuration getConfiguration() {
-        return new org.neo4j.ogm.config.Configuration.Builder()
+    public Configuration getConfiguration() {
+        return new Configuration.Builder()
                 .uri(System.getenv("GRAPHENEDB_URL"))
                 //.credentials(System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD"))
                 .build();

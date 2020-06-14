bcf16847aee7b88c949d3beb41a67d3b870831da
AdamMCG97 <adammorley8@aol.com>
2020-06-14 18:27:31 +0100

    Migrate to http driver from bolt driver

--- src/main/java/tech/amcg/LLF/configuration/Neo4JConfiguration.java
+++ src/main/java/tech/amcg/LLF/configuration/Neo4JConfiguration.java
@@ -2,20 +2,22 @@ package tech.amcg.llf.configuration;
 
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import tech.amcg.llf.repository.TubeRepository;
 
 @Configuration
-@EnableNeo4jRepositories(basePackages="tech.amcg.llf")
+@ComponentScan("tech.amcg.llf")
+@EnableNeo4jRepositories(basePackages="tech.amcg.llf.repository")
 public class Neo4JConfiguration {
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
         return new org.neo4j.ogm.config.Configuration.Builder()
-                .uri(System.getenv("GRAPHENEDB_BOLT_URL"))
-                .credentials(System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD"))
+                .uri(System.getenv("GRAPHENEDB_URL"))
+                //.credentials(System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD"))
                 .build();
     }
 
@@ -26,10 +28,7 @@ public class Neo4JConfiguration {
 
     @Bean
     public SessionFactory sessionFactory() {
-        return new SessionFactory(new org.neo4j.ogm.config.Configuration.Builder()
-                .uri(System.getenv("GRAPHENEDB_BOLT_URL"))
-                .credentials(System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD"))
-                .build(), "tech.amcg.llf");
+        return new SessionFactory(getConfiguration(), "tech.amcg.llf");
     }
 
 /*    @Bean

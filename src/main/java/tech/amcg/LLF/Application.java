b131cd66f2c5ed7d1fdd62a30bb17ea3095c34aa
AdamMCG97 <adammorley8@aol.com>
2020-06-14 18:49:05 +0100

    Remove neo4j configuration file

--- src/main/java/tech/amcg/LLF/Application.java
+++ src/main/java/tech/amcg/LLF/Application.java
@@ -2,8 +2,6 @@ package tech.amcg.llf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class Application {
